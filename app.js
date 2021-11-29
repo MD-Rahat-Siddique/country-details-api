a8c120d61f92c2c470a3d24da086397fc77f55fc
Rahat Siddique <rahatsiddique2717@gmail.com>
2021-11-30 00:27:04 +0600

    final commit

--- app.js
+++ app.js
@@ -11,25 +11,32 @@ const displayCountries = countries => {
         const countryInfo = `
             <h3 class="country-name">${country.name.common}</h3>
             <p>${country.capital}</p>
+            <button onclick="countryDetail('${country.name.common}')">Details</button>
         `;
-
 
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
     });
+}
 
-    // for (let i = 0; i < countries.length; i++) {
-    //     const country = countries[i];
-    //     const  countryDiv = document.createElement('div');
-    //     countryDiv.className = 'country';
+const countryDetail = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => countryInfo(data[0]));
 
-    //     const countryInfo = `
-    //         <h3 class="country-name">${country.name.common}</h3>
-    //         <p>${country.capital}</p>
-    //     `;
+    document.getElementById('countries').style.display = "none";
+    document.getElementById('countryDetail').style.display = 'block';
+}
 
-
-    //     countryDiv.innerHTML = countryInfo;
-    //     countriesDiv.appendChild(countryDiv);
-    // }
+const countryInfo = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('countryDetail');
+    countryDiv.innerHTML = `
+        <h1>Name : ${country.name.common}</h1>
+        <p>Population : ${country.population}</p>
+        <p>Area : ${country.area}</p>
+        <img src="${country.flags.png}" alt="">
+        <button onclick="document.location='index.html'" class="back-button">Back</button>
+    `
 }
